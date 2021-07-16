e71cdc95a86096914275384deba0532d382b0a0c
Eddie Jung <ejung716@gmail.com>
2021-07-16 14:27:45 -0700

    First commit

--- NBAstats/ViewController.swift
+++ NBAstats/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let labelRect = CGRect(x: 50, y: 100, width: 200, height: 100)
+        let label = UILabel(frame: labelRect)
+        label.text = "Hello World"
+        view.addSubview(label)
+        
     }
 
-
+    
 }
 
